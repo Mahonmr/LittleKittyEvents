4871443e6cbce512426748c8edc7156564158bda
mike <Guest@epicodus-50.local>
2016-03-31 16:36:14 -0700

    more steps adding cancancan to app

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,18 +2,35 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    user ||= User.new # guest user (not logged in)
     if user.admin?
       can :manage, :all
     elsif user.athlete?
-      can :read, User
-      can :update, User do |edit_user|
-        edit_user.try(:id) == user.id
-      end
-      can :edit, User do |edit_user|
-        edit_user.try(:id) == user.id
-      end
+      self_only(:read, User, user)
+      self_only(:edit, User, user)
+      self_only(:update, User, user)
+      can :create, Club
+      can :read, Club
+      user_rights(:edit, Club, user)
+      user_rights(:update, Club, user)
+      user_rights(:destroy, Club, user)
+      cannot [:index], User
     else
-      can :read, :all
+      can :read, Club
+    end
+  end
+
+private
+
+  def self_only(action, model, user)
+    can action, model do |member|
+      member.try(:id) == user.id
+    end
+  end
+
+  def user_rights(action, model, user)
+    can action, model do |member|
+      member.try(:user) == user
     end
   end
 end
